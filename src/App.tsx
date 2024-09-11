231ee10db297f87ee0d333866711374e44ce8e26
ntl-alyan <alyan.quddoos@nayatel.com>
2024-09-11 15:16:06 +0500

    Route set to default

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-        <Route path="Products" element={<Products/>} />
+        <Route path="/" element={<Products/>} />
       </Routes>
       </BrowserRouter>
     </div>
